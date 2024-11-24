0f26cc8cd6a0de4fdfb93d59d7ebaaacd851ed2f
RafaPear <rafapear@outlook.pt>
2024-11-24 01:44:13 +0000

    Lets go we can draw a snake on the screen now (she doesn't move yet, wait for that guys it will happen soon)

--- src/main/kotlin/World.kt
+++ src/main/kotlin/World.kt
@@ -1,15 +1,20 @@
+/**
+ * A Vector2 is a two-dimensional type that supports
+ * operations like adding a constant
+ */
+
 data class Vector2(var x: Int, var y: Int) {
-    operator fun plus(a: Int) =
-        Vector2(x+ a, y + a)
+    operator fun plus(a: Int) = Vector2(x + a, y + a)
+    operator fun plus(a: Vector2) = Vector2(x + a.x, y + a.y)
 
-    operator fun minus(a: Int) =
-        Vector2(x - a, y - a)
+    operator fun minus(a: Int) = Vector2(x - a, y - a)
+    operator fun minus(a: Vector2) = Vector2(x - a.x, y - a.y)
 
-    operator fun times(a: Int) =
-        Vector2(x * a, y * a)
+    operator fun times(a: Int) = Vector2(x * a, y * a)
+    operator fun times(a: Vector2) = Vector2(x * a.x, y * a.y)
 
-    operator fun div(a: Int) =
-        Vector2(x / a, y / a)
+    operator fun div(a: Int) = Vector2(x / a, y / a)
+    operator fun div(a: Vector2) = Vector2(x / a.x, y / a.y)
 }
 
 val UP = 0
